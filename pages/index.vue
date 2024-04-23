35af483c313fae37618e779a451928df22c33be8
GitCityT <452896643@qq.com>
2024-04-23 17:11:45 +0900

    lint test 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <div>this is the index page;</div>
+  <div @click="incr('123')">this is the index page{{ loading }};</div>
 </template>
+<script setup>
+import { ref } from "vue";
+const route = useRoute();
+
+const loading = ref("");
+
+// When accessing /posts/1, route.params.id will be 1
+console.log(route);
+
+function incr(str) {
+  loading.value = str + "1";
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  font-size: 14;
+}
+</style>
